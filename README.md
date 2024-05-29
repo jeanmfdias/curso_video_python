20135fed208af28712509942084016de15bddfc6
jeanmfdias <jeanmfdias@gmail.com>
2024-05-29 19:03:44 -0300

    Adicionado exercicio 03

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Imprimir na tela "Olá Mundo!"
 2. Receber um nome digitado pelo usuário, e imprimir com a mensagem "É um prazer te conhecer {NOME}"
+3. Receber dois valores e exibir a soma deles
---
+++ ex003.py
@@ -0,0 +1,6 @@
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+soma = int(numero1) + int(numero2)
+
+print('A soma de {} + {} = {}'.format(numero1, numero2, soma))
