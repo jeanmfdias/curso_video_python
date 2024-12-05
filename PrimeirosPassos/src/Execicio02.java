b1053e311a5738f17031808be454d08ab1545a69
jeanmfdias <jeanmfdias@gmail.com>
2024-12-05 19:04:13 -0300

    Criado exercicio 02

---
+++ PrimeirosPassos/src/Execicio02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Execicio02 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String nome = leitor.nextLine();
+
+        System.out.println("Bem vindo " + nome);
+    }
+}
--- PrimeirosPassos/src/Exercicio01.java
+++ PrimeirosPassos/src/Exercicio01.java
@@ -1,4 +1,4 @@
-public class OlaMundo {
+public class Exercicio01 {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
     }
